ea025cd3d2d58a03fcf47c9b2aa5bde0dd4cf3d3
dieguitocano <dieguito.kano@gmail.com>
2021-10-04 18:43:46 -0500

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Prints lowercase alphabet in reverse
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c-)
+{
+putchar(c);
+}
+putchar('\n');
+
+return (0);
+}
