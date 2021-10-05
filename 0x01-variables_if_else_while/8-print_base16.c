3c30c625f84ab39e0edff88e57122b29a6aae89e
dieguitocano <dieguito.kano@gmail.com>
2021-10-04 21:54:04 -0500

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,21 +3,21 @@
 #include <time.h>
 
 /**
- *main - Prints all numbers of base 16 in lowercase
+ * main - main block
  *
- *Return: o
+ *Return: 0
  *
  */
 int main(void)
 {
 char c;
 int i;
-for(i = 0; i < 10; i++)
+
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+for (c = 'a'; c < 'g'; c++)
 {
 putchar(c);
 }
-putchar('\n');
-
-return (0);
-}
-
