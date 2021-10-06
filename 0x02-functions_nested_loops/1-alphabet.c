1c59a6179179835993ccf87d332c44ee5bc62d44
dieguitocano <dieguito.kano@gmail.com>
2021-10-06 18:35:09 -0500

    Suffers from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Prints alphabet
+ *
+ *Return: 0
+ */
+void print_alphabet(void)
+{
+int d;
+
+for (d = 97; d < 123; d++)
+{
+_putchar(d);
+}
+_putchar('\n');
+}
