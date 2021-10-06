a3d06f6c2c8d095e75a7088d9a4e2a4e331093aa
dieguitocano <dieguito.kano@gmail.com>
2021-10-05 21:59:04 -0500

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - 
+ *Return: o
+ */
+int main(void)
+{
+char c[] = "_putchar";
+int d;
+
+for (d = 0; d <= 7; d++)
+{
+_putchar(c[d]);
+}
+_putchar('\n');
+return (0);
+}
